ae5a2f0e9c6554393b90014ce65bed6424a305fe
Edwin Julaton <denzndhauz@users.noreply.github.com>
2016-04-14 12:53:55 +0800

    Added files via upload
    update v 0.0.1

---
+++ main/index.php
@@ -0,0 +1,285 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Maanyag Pension House and Spa; Mandaue City, Cebu">
+    <meta name="author" content="">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.ico"/>
+
+    <title>Home - Maanyag Pension House and Spa</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="dist/css/carousel.css" rel="stylesheet">
+    <link href="dist/css/bootstrap-social.css" rel="stylesheet">
+    <link href="dist/css/font-awesome.css" rel="stylesheet">
+    <link href="dist/css/style.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:700|Dancing+Script|Noto+Serif' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+   <!-- Fixed navbar -->
+    <nav class="navbar nav-eff navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">
+              <img class="logo" src="img/maanyagmainsmall.png" alt="Maanyag Pension House and Spa">
+              <p class="brand-title">Pension House and Spa</p>
+          </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <!--
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li> -->
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+	<!-- Carousel
+	================================================== -->
+	<div id="carousel-example-generic" id="myCarousel" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	    <li data-target="#myCarousel" data-slide-to="1"></li>
+	    <li data-target="#myCarousel" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="item active">
+	      <img src="img/slide1-960x420-t.png" style="width:100%;" class="img-responsive">
+	      <div class="container">
+	        <div class="carousel-caption">
+	          <b style="font-family: 'Dancing Script', cursive;">Welcome to Maanyag Pension House Web Portal</b>
+	        </div>
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="img/slide2-960x420.png" style="width:100%" class="img-responsive">
+	      <div class="container">
+	        <div class="carousel-caption">
+	          <b>Changes to the Grid</b>
+	        </div>
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="img/slide3-960x420-t.png" style="width:100%" class="img-responsive">
+	      <div class="container">
+	        <div class="carousel-caption">
+	          <b>Percentage-based sizing</b>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+	</div>
+	<!-- /.carousel -->
+
+
+<!-- Marketing messaging and featurettes
+================================================== -->
+<!-- Wrap the rest of the page in another container to center all the content. -->
+<div class="container">
+  <div class="row quote">
+    <div class="col-md-12">
+      <blockquote style="font-family: 'Dancing Script', cursive;">
+        <h1><center><i class="fa fa-quote-left fa-1x" aria-hidden="true"></i> At Maanyag, our commitment is to provide excellent quality of service by offering a mix of warm hospitality and delightful personal service to our clients.<i class="fa fa-quote-right fa-1x" aria-hidden="true"></i></center></h1>
+      </blockquote>
+    </div>
+  </div>
+</div>
+<div class="container marketing" style="font-family: 'Noto Serif', serif;">
+  <!-- Three columns of text below the carousel -->
+  <div class="row">
+    <div class="page-header">
+      <h1>Our Services</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="http://placehold.it/242x200" alt="...">
+        <div class="caption">
+          <h3>Pension House</h3>
+          <p>
+            <ul id="list" style="list-style: none; display: inline-block; margin-left: -30px">
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Fully Air Conditioned Rooms</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Private Toilet &amp; Bath Rooms</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 24 hrs Security</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> CCTV Monitoring System</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Cable Television</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Clean Water Supply</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Wifi Internet Access</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Laundry Service</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Room Service</li>
+              <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Car Park</li>
+            </ul>
+          </p>
+          <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="img/sub-title-barber-t.jpg" alt="...">
+        <div class="caption">
+          <h3>Salon &amp; Barber Shop</h3>
+          <p>...</p>
+          <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="img/sub-title-spa-t.jpg" alt="...">
+        <div class="caption">
+          <h3>Spa</h3>
+          <p>...</p>
+          <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--
+  <div class="row">
+    <div class="col-md-4 text-center">
+      <img class="img-circle" src="http://placehold.it/140x140">
+      <h2>Pension House</h2>
+      <p>Tablets, phones, laptops. The new 3 promises to be mobile friendly from the start.</p>
+      <p><a class="btn btn-default" href="#">View details »</a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img class="img-round" src="img/sub-title-spa-t.jpg">
+      <h2>Spa</h2>
+      <p>There is now just one percentage-based grid for Bootstrap 3. Customize for fixed widths.</p>
+      <p><a class="btn btn-default" href="#">View details »</a></p>
+    </div>
+    <div class="col-md-4 text-center">
+      <img class="img-round" src="img/sub-title-spa-t.jpg">
+      <h2>Salon and Barber Shop</h2>
+      <p>Improved support for mixins make the new Bootstrap 3 easier to customize.</p>
+      <p><a class="btn btn-default" href="#">View details »</a></p>
+    </div>
+  </div><!-- /.row -->
+
+  <hr>
+
+  <!-- FOOTER -->
+  <footer>
+    <p class="pull-right"><a href="#">Back to top</a></p>
+    <p>&copy; Maanyag Pension House and Spa 2016</p>
+    <a href="#" class="btn btn-social-icon btn-facebook btn-xs">
+        <span class="fa fa-facebook"></span>
+    </a> 
+    <a href="#" class="btn btn-social-icon btn-twitter btn-xs">
+        <span class="fa fa-twitter"></span>
+    </a> 
+    / <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>  maanyag.online@yahoo.com 
+    <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> <b>232-6220</b> | <b>232-3699</b> | <b>236-1505</b>
+  </footer>
+
+  <div class="float-menu">
+  	
+  </div>
+
+  <!-- Modal for BOOKING -->
+  <div class="modal fade" id="booking" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
+    <div class="modal-dialog modal-sm" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="gridSystemModalLabel">Enter Book Date</h4>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>
+                  From
+                </label>
+                <input type="date" class="form-control" name="start_date">
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group">
+                <label>
+                  To
+                </label>
+                <input type="date" class="form-control" name="start_date">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Proceed</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
+</div><!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="dist/js/jquery-1.11.0.min.js"></script>
+    <script src="dist/js/script.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="dist/js/bootstrap.min.js"></script>
+
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
